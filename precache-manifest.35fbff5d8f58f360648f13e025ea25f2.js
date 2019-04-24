2c8b4993008e6cc9b52e8b08441a80fe6a3b7cd4
YoniR44 <taniratz@hotmail.com>
2019-04-24 17:41:13 +0300

    Updates

---
+++ precache-manifest.35fbff5d8f58f360648f13e025ea25f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d40f597058a2087bd75",
+    "url": "/bitcoin-react/static/js/runtime~main.3674c0ff.js"
+  },
+  {
+    "revision": "e216f167b15680f402d3",
+    "url": "/bitcoin-react/static/js/main.6314bf41.chunk.js"
+  },
+  {
+    "revision": "adbb940df2f876443fec",
+    "url": "/bitcoin-react/static/js/2.1cd57f2f.chunk.js"
+  },
+  {
+    "revision": "e216f167b15680f402d3",
+    "url": "/bitcoin-react/static/css/main.a898ea7b.chunk.css"
+  },
+  {
+    "revision": "34e60db712434af62261cd6151d8506e",
+    "url": "/bitcoin-react/index.html"
+  }
+];
